40b4c8366e29f88a04e92874b7f5d3c3527d04ee
James Aung <therealaung@gmail.com>
2017-09-20 17:08:03 +0100

    add first and last name to devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users,
-    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
   root to: 'pages#home'
   resources :tickets, only: [:create, :destroy, :show] do
     resources :comments, only: [:create, :destroy]
