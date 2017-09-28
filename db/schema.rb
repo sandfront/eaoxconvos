f130614ed53de1d325252a6158e5fab83df1e408
James Aung <therealaung@gmail.com>
2017-09-28 13:58:12 +0100

    add tickbox to ticket form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920150052) do
+ActiveRecord::Schema.define(version: 20170928124043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170920150052) do
     t.datetime "updated_at", null: false
     t.integer "acceptor_id"
     t.boolean "permanent", default: false, null: false
+    t.boolean "anonymous", default: false, null: false
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
